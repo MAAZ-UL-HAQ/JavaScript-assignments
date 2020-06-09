7e23e8c59bde644d2938cf06973f170027d8c9f3
maazulhaq <maazulhaq00@gmail.com>
2020-06-10 03:36:48 +0500

    Update

---
+++ Ch 1-30/app.js
@@ -0,0 +1,31 @@
+// ***************************** Chapter 1 **************************** 
+
+// ****************************** Task 1 ******************************
+
+// alert("Hello! World");
+
+// ****************************** Task 2 ******************************
+
+// PW=prompt("Enter Password");
+// if(PW === "wm-5035")
+// {
+//     alert("Thank you! Now you are authorized");
+// }
+// else
+// {
+//     alert("Error! Please Enter a valid password");
+// }
+
+// ****************************** Task 3 ******************************
+
+// alert("Welcome to JS Land... \n Happy Coding!");
+
+// ****************************** Task 4 ******************************
+
+// alert("Welcome to JS Land... ");
+// alert("Happy Coding!");
+
+// ****************************** Task 5 ******************************
+
+// alert("Hello! I can run JS throught my web browser's console ");
+
