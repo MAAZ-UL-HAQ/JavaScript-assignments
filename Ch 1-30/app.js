e56801df63b87b3676bdcd637f0f82ed44daf7c9
maazulhaq <maazulhaq00@gmail.com>
2020-06-11 02:08:11 +0500

    Update

--- Ch 1-30/app.js
+++ Ch 1-30/app.js
@@ -449,8 +449,91 @@
 // }
 
 // ****************************** Task 5 ******************************
+// // a.
+// var a = 4;
+// if (++a === 5) {
+//     alert("given condition for variable a is true");
+// }
+
+// // b.  
+// var b = 82;
+// if (b++ === 83) {
+//     alert("given condition for variable b is true");
+// }
+
+// c.  
+// var c = 12;
+// if (c++ === 13) {
+//     alert("condition 1 is true");
+// }
+// if (c === 13) {
+//     alert("condition 2 is true");
+// } 
+// if (++c < 14) { 
+//     alert("condition 3 is true");
+// } 
+// if (c === 14) { 
+//     alert("condition 4 is true"); 
+// }
+
+// // d.  
+// var materialCost = 20000; 
+// var laborCost = 2000; 
+// var totalCost = materialCost + laborCost; 
+// if (totalCost === laborCost + materialCost) { 
+//     alert("The cost equals"); 
+// }
+
+// // e.  
+// if (true) { 
+//     alert("True"); 
+// } 
+// if (false) { 
+//     alert("False"); 
+// }
+
+// // f.  
+// if ("car" < "cat") { 
+//     alert("car is smaller than cat"); 
+// }
 
 // ****************************** Task 6 ******************************
+var totalMarksPerSub = Number(prompt("Enter total marks of a test"));
+var subject1=Number(prompt("Enter obtained marks of 1st subject"));
+var subject2=Number(prompt("Enter obtained marks of 2nd subject"));
+var subject3=Number(prompt("Enter obtained marks of 3rd subject"));
+var grandTotal=totalMarksPerSub*3;
+var obtainedTotal=subject1+subject2+subject3;
+var percentage =(obtainedTotal/grandTotal*100).toFixed(2);
+var grade = "";
+var remarks = "";
+
+if(percentage>=80 && percentage<=100){
+    grade="A-one";
+    remarks="Excellent";
+}
+else if(percentage>=70 && percentage<80){
+    grade="A";
+    remarks="Good";
+}
+else if(percentage>=60 && percentage<70){
+    grade="B";
+    remarks="Need to improve";
+}
+else if(percentage>=50 && percentage<60){
+    grade="Fail";
+    remarks="Sorry";
+}
+else{
+    remarks="Sorry! You provide incorrect information";
+}
+
+document.write("<h1>Marks Sheet</h1>"+"<br>");
+document.write("<h3>Total Marks    : " + grandTotal + "<h3>");
+document.write("<h3>Obtained Marks : " + obtainedTotal + "<h3>");
+document.write("<h3>Percentage     : " + percentage + "% <h3>");
+document.write("<h3>Grade          : " + grade + "<h3>");
+document.write("<h3>Remarks        : " + remarks + "<h3>");
 
 // ****************************** Task 7 ******************************
 
@@ -461,7 +544,7 @@
 // ****************************** Task 10 ******************************
 
 // ****************************** Task 11 ******************************
- 
+
 
 
 
