9867f4cd32a2f30ca62fbbd852bfd00e03d8a9e7
maazulhaq <maazulhaq00@gmail.com>
2020-06-15 23:57:06 +0500

    Update

--- Ch 21-38/app.js
+++ Ch 21-38/app.js
@@ -16,15 +16,41 @@
 
 // ****************************** Task 3 ******************************
 
-var string = "Pakistani"
-var i = string.indexOf("n");
-document.write("String is : "+string+"<br>");
-document.write("Index of 'n' is : "+i);
+// var string = "Pakistani"
+// var i = string.indexOf("n");
+// document.write("String is : "+string+"<br>");
+// document.write("Index of 'n' is : "+i);
 
 // ****************************** Task 4 ******************************
+
+
+// var string = "Hello World"
+// var i = string.indexOf("l");
+// document.write("String is : "+string+"<br>");
+// document.write("Index of 'l' is : "+i);
+
 // ****************************** Task 5 ******************************
+
+
+// var string = "Pakistani"
+// var char = string.charAt(3);
+// document.write("String is : "+string+"<br>");
+// document.write("Character at index 3 : "+char);
+
 // ****************************** Task 6 ******************************
+
+
+// var char = string.charAt(3);
+// document.write("String is : "+string+"<br>");
+// document.write("Character at index 3 : "+char);
+
 // ****************************** Task 7 ******************************
+
+// var fname = prompt("Enter your first name : ");
+// var lname = prompt("Enter your last name : ");
+// var fullName = fname.concat(" ").concat(lname);
+// alert("Hello! "+fullName.toUpperCase());
+
 // ****************************** Task 8 ******************************
 // ****************************** Task 9 ******************************
 // ****************************** Task 10 ******************************
