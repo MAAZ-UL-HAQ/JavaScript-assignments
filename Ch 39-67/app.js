5f7b5bedb4ceb5ec20bacf264fc7d995a71868d7
maazulhaq <maazulhaq00@gmail.com>
2020-07-15 14:41:19 +0500

    Update

--- Ch 39-67/app.js
+++ Ch 39-67/app.js
@@ -101,26 +101,157 @@
 
 // ****************************** Task 6 ******************************
 
-function delVowel(string){
-    for(var i=0;i<string.length;i++){
-        if(string[i]=="a" || string[i]=="e" || string[i]=="i" || string[i]=="o" || string[i]=="u" || string[i]=="A" || string[i]=="E" || string[i]=="I" || string[i]=="O" || string[i]=="U" ){
-            string=string.slice(0,i)+string.slice(i+1);
-            i--
-        }
-    }
-    return string
-}
+// function delVowel(string){
+//     for(var i=0;i<string.length;i++){
+//         if(string[i]=="a" || string[i]=="e" || string[i]=="i" || string[i]=="o" || string[i]=="u" || string[i]=="A" || string[i]=="E" || string[i]=="I" || string[i]=="O" || string[i]=="U" ){
+//             string=string.slice(0,i)+string.slice(i+1);
+//             i--
+//         }
+//     }
+//     return string
+// }
 
-var text = prompt("Input string to delete vowel:")
-var withoutVowel = delVowel(text);
+// var text = prompt("Input string to delete vowel:")
+// var withoutVowel = delVowel(text);
 
-alert("Vowel are deleted :).. \nOld string: \'" + text + "\'\nNew string: \'"+withoutVowel+"\'");
+// alert("Vowel are deleted :).. \nOld string: \'" + text + "\'\nNew string: \'"+withoutVowel+"\'");
 
 // ****************************** Task 7 ******************************
 
+// var occurance = []
+// function countVowelPair(text) {
+//     text = text.toLowerCase;
+//     var count = 0;
+//     for (var i = 0; i < text.length; i++) {
+//         switch (text.slice(i, i + 2)) {
+//             case "aa":
+//                 count++ ; occurance.push("aa");
+//                 break;
+//             case "ae":
+//                 count++ ; occurance.push("ae");
+//                 break;
+//             case "ai":
+//                 count++ ; occurance.push("ai");
+//                 break;
+//             case "ao":
+//                 count++ ; occurance.push("ao");
+//                 break;
+//             case "au":
+//                 count++ ; occurance.push("au");
+//                 break;
+//             case "ea":
+//                 count++ ; occurance.push("ea");
+//                 break;
+//             case "ee":
+//                 count++ ; occurance.push("ee");
+//                 break;
+//             case "ei":
+//                 count++ ; occurance.push("ei");
+//                 break;
+//             case "eo":
+//                 count++ ; occurance.push("eo");
+//                 break;
+//             case "eu":
+//                 count++ ; occurance.push("eu");
+//                 break;
+//             case "ia":
+//                 count++ ; occurance.push("ia");
+//                 break;
+//             case "ie":
+//                 count++ ; occurance.push("ie");
+//                 break;
+//             case "ii":
+//                 count++ ; occurance.push("ii");
+//                 break;
+//             case "io":
+//                 count++ ; occurance.push("io");
+//                 break;
+//             case "iu":
+//                 count++ ; occurance.push("iu");
+//                 break;
+//             case "oa":
+//                 count++ ; occurance.push("oa");
+//                 break;
+//             case "oe":
+//                 count++ ; occurance.push("oe");
+//                 break;
+//             case "oi":
+//                 count++ ; occurance.push("oi");
+//                 break;
+//             case "oo":
+//                 count++ ; occurance.push("oo");
+//                 break;
+//             case "ou":
+//                 count++ ; occurance.push("ou");
+//                 break;
+//             case "ua":
+//                 count++ ; occurance.push("ua");
+//                 break;
+//             case "ue":
+//                 count++ ; occurance.push("ue");
+//                 break;
+//             case "ui":
+//                 count++ ; occurance.push("ui");
+//                 break;
+//             case "uo":
+//                 count++ ; occurance.push("uo");
+//                 break;
+//             case "uu":
+//                 count++ ; occurance.push("uu");
+//                 break;
+//             default:
+//                 break;
+//         }
+//     }
+//     return count;
+// }
+
+// var s = prompt("Enter string to count pairs of successive vowels:");
+// var c = countVowelPair(s);
+
+// alert("String : \'" + s + "\'\nPair count = " + c + "\noccurance : " + occurance);
+
 // ****************************** Task 8 ******************************
 
+// function kmToMeter(km){
+//     return (km*1000);
+// }
+// function kmToCentimeter(km){
+//     return (km*1000*100)
+// }
+// function kmToFeet(km){
+//     var feet = kmToCentimeter(km)/31;
+//     return feet;
+// }
+// function kmToInches(km){
+//     var inches = kmToFeet(km)*12
+//     return inches;
+// }
+
+// var km = prompt("Enter distance between two cities (in km)","100");
+// var m = kmToMeter(km);
+// var cm = kmToCentimeter(km);
+// var f = kmToFeet(km);
+// var i = kmToInches(km);
+
+// alert("distance Km          : "+km+"\ndistance in meters : "+m+"\ndistance in cm      : "+cm+"\ndistance in feets   : "+f+"\ndistance in inches : "+i);
+
 // ****************************** Task 9 ******************************
+
+var totalHours = Math.round(+prompt("Enter total number of work hours (of whole week) of employee"));
+var otPay = overtimePay(totalHours)
+alert("Overtime Pay = Rs."+otPay);
+
+function overtimePay(totalHrs){
+    var overtimePay;
+    if(totalHours > 40){
+        overtimePay = (totalHrs-40)*12;
+    }
+    else{
+        overtimePay=0
+    }
+    return overtimePay;
+}
 
 // ****************************** Task 10 ******************************
 
